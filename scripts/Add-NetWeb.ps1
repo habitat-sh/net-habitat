06cee7bbe16d6737ace60e573f87b65ecc877c9a
Matt Wrock <matt@mattwrock.com>
2016-10-21 10:44:12 -0700

    add version and precreate asp.net temp directory

--- scripts/Add-NetWeb.ps1
+++ scripts/Add-NetWeb.ps1
@@ -1,8 +1,11 @@
 $appName = "net-web2"
 $port = 8080
 
+mkdir '$PSScriptRoot\..\framework64\v4.0.30319\Temporary ASP.NET Files'
+
 if(!(Test-Path "IIS:\AppPools\$appName\")) {
     New-WebAppPool -Name $appName
+    Set-ItemProperty IIS:\AppPools\$appName -name processModel -value @{setProfileEnvironment="true"}
 }
 
 if(!(Test-Path "IIS:\Sites\$appName\")) {
@@ -20,6 +23,9 @@ if(!(Test-Path HKU:\)){
 }
 if(!(Get-ItemProperty -Path HKU:\$sid\Environment -Name COMPLUS_InstallRoot -ErrorAction SilentlyContinue)){
     New-ItemProperty -Path HKU:\$sid\Environment -Name COMPLUS_InstallRoot -Value (Resolve-Path "$PSScriptRoot\..\framework64")
+}
+if(!(Get-ItemProperty -Path HKU:\$sid\Environment -Name COMPLUS_version -ErrorAction SilentlyContinue)){
+    New-ItemProperty -Path HKU:\$sid\Environment -Name COMPLUS_version -Value "v4.0.30319"
 }
 if(!(Get-ItemProperty -Path HKU:\$sid\Environment -Name DEVPATH -ErrorAction SilentlyContinue)){
     New-ItemProperty -Path HKU:\$sid\Environment -Name DEVPATH -Value (Resolve-Path "$PSScriptRoot\..\net-apps\net-web\assemblies")
